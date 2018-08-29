fe5101332ddb62d68d80e334e3f821bc9d6cc544
RossPin <dorpetz@hotmail.com>
2018-08-30 11:42:56 +1200

    setup react

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,4 @@
 const Router = require('express').Router()
 
-Router.get('/', (req, res) => {
-    res.send('Hello World')
-})
 
 module.exports = Router
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const server = express()
 const routes = require('./routes/routes')
 const path = require('path')
 
-server.use('/', routes)
+//server.use('/', routes)
 server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
