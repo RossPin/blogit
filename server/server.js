632b7870319dda3edbecec6f8afb75234440b557
RossPin <dorpetz@hotmail.com>
2018-08-30 11:00:47 +1200

    use public folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const server = express()
 const routes = require('./routes/routes')
+const path = require('path')
 
 server.use('/', routes)
+server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
