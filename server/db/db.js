0cb5908a196127da0cb6739dec1bc8335c492847
RossPin <dorpetz@hotmail.com>
2018-09-04 11:16:26 +1200

    add comments field

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/blog-test")
 
-var postSchema = new mongoose.Schema({ title: String, text: String })
+var postSchema = new mongoose.Schema({ title: String, text: String, comments: Array})
 var Post = mongoose.model('Post', postSchema)
 
 function addPost(post) {
+  post.comments = ['comment']
   var postData = new Post(post)  
   return postData.save()
 }
